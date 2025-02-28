23c08262be09485f067c0ced398585de7bf22e68
Hayden Steele <haydenallensteene@gmail.com>
2025-02-28 15:26:52 -0700

    added calendar

--- display.py
+++ display.py
@@ -1,6 +1,6 @@
 import sys
 from zoneinfo import ZoneInfo
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageOps
 
 import logging
 import os
@@ -15,6 +15,7 @@ Smono = ImageFont.truetype('./SourceCodePro-VariableFont_wght.ttf', 20, encoding
 Mmono = ImageFont.truetype('./SourceCodePro-VariableFont_wght.ttf', 30, encoding="unic")
 Bmono = ImageFont.truetype('./SourceCodePro-VariableFont_wght.ttf', 40, encoding="unic")
 subFont = ImageFont.truetype('./LexendGiga-VariableFont_wght.ttf', 30, encoding="unic")
+subSubFont = ImageFont.truetype('./LexendGiga-VariableFont_wght.ttf', 20, encoding="unic")
 
 
 import pandas as pd
@@ -27,16 +28,16 @@ import math
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-import asyncio
 import PIL.ImageOps  
+import calendar
 
-# from mcstatus import JavaServer, BedrockServer
+from mcstatus import JavaServer, BedrockServer
 
 # logging.basicConfig(level=logging.DEBUG)
 
-async def main():
+def main():
 
-    image = await genImage()
+    image = genImage()
 
     if (platform.system() == "Darwin"):
         image = image.save("out.jpg")
@@ -56,13 +57,14 @@ async def main():
 
 
 
-async def genImage(width=800, height=480):
+def genImage(width=800, height=480):
     Himage = Image.new('1', (width, height), 255)
 
-    data = await getWeather()
-    Himage.paste(data)
-
     draw = ImageDraw.Draw(Himage)
+    
+    getCalendar(draw)
+    getWeather(draw)
+    getMCStatus(draw)
 
     now = datetime.now()
     day_name = now.strftime("%A")  # Full weekday name
@@ -73,27 +75,17 @@ async def genImage(width=800, height=480):
 
     draw.text((15, 80), day_name, 'black', subFont)
     draw.text((10, 0), date_str, 'black', Bfont)
-    draw.text((800, 5), "Refreshed at:", 'black', SSmono, anchor="rt")
-    draw.text((790, 25), time_str, 'black', SSmono, anchor="rt")
+    draw.line((10, 120, 300, 120), fill='black', width=2)
 
-    # draw.line((10, 130, 300, 130), fill='black', width=2)
+    draw.text((5, 475), f"Refreshed at: {time_str}", 'black', SSmono, anchor="lb")
 
     return Himage
 
 
 
 
-
-async def getWeather():
+def getWeather(draw: ImageDraw.ImageDraw):
     
-    
-
-    # Make images
-    Himage = Image.new('1', (800, 480), 255)
-    draw = ImageDraw.Draw(Himage)
-
-
-    # Recomment back in
 
     if (len(sys.argv) == 1):
         print("Fetching Weather")
@@ -119,21 +111,126 @@ async def getWeather():
 
     day = day.replace(current, "")
     SSmono.set_variation_by_name("Regular")
-    draw.text((400, 240), day, "black", SSmono, anchor="mm")
+    draw.text((400, 360), day, "black", SSmono, anchor="mm")
 
+def getCalendar(draw: ImageDraw.ImageDraw):
+
+    x = 550
+    y = 40
+
+    width = 250
+    height = 170
+
+    i = 0
+
+    now = datetime.now()
+    month = calendar.monthcalendar(now.year, now.month)
+    today = now.day
+
+    days = [ "S", "M", "T", "W", "T", "F", "S" ]
+
+    draw.line((x - 10, y + 12, x + width - 25, y + 12), "black", 1)
+
+    subFont.set_variation_by_name("Regular")
+
+    for day in days:
+        draw.text((
+            x + ( (i % 7) * ( 1 / 7 ) * width ), 
+            y + ( (i // 7) * ( 1 / len(month) * height ) )
+        ), day, "black", SSmono, anchor="mm")
+
+        i += 1
+
+    for row in month:
+        for day in row:
+
+            i += 1
+
+            if (day == 0): 
+                continue
+
+            if (day == today):
+
+                SSmono.set_variation_by_name("Bold")
+
+                # draw.circle((
+                #     x + ( (i % 7) * ( 1 / 7 ) * width ), 
+                #     y + ( (i // 7) * ( 1 / len(month) * height ) + 1)
+                # ), 15, "white", "black", 1)
+
+                draw.line(( 
+                    x + ( (i % 7) * ( 1 / 7 ) * width ) - 10, 
+                    y + ( (i // 7) * ( 1 / len(month) * height ) ) + 10,
+                    x + ( (i % 7) * ( 1 / 7 ) * width ) + 10, 
+                    y + ( (i // 7) * ( 1 / len(month) * height ) ) + 10
+                ), "black", 2)
+
+            else:   
+
+                SSmono.set_variation_by_name("Light")
+
+            draw.text((
+                x + ( (i % 7) * ( 1 / 7 ) * width ), 
+                y + ( (i // 7) * ( 1 / len(month) * height ) )
+            ), str(day), "black", SSmono, anchor="mm")
+
+def getMCStatus(draw: ImageDraw.ImageDraw):
+
+
+    x = 10
+    y = 140
+
+
+    jLatency = -1
+    bLatency = -1
+
+    try: 
+
+        ip = "minecraft.steeleinnovations.com"
+        jServer = JavaServer.lookup(ip)
+        bServer = BedrockServer.lookup(ip)
+
+        jLatency = round(jServer.status().latency, 1)
+        bLatency = round(bServer.status().latency, 1)
     
-    # server = "minecraft.steeleinnovations.com"
-    # javaStatus = JavaServer.lookup(server)
-    # bedrockStatus = BedrockServer.lookup(server)
+    except: 
+        pass
 
-    # javaStatus.status().
+    draw.text((x + 5, y + 5), "Java:", "black", subSubFont)
+    draw.text((x + 5, y + 40), "Bedrock:", "black", subSubFont)
+
+    if (jLatency == -1):
+        
+        draw.rectangle((
+            x + 130, 
+            y, 
+            x + 240, 
+            y + 37
+        ), "black")
+
+        draw.text((x + 140, y + 7), "Offline", "white", subSubFont)
+
+    else:
+        draw.text((x + 140, y + 7), "Online", "black", subSubFont)
 
 
-    return Himage
+    if (bLatency == -1):
+        
+        draw.rectangle((
+            x + 130, 
+            y + 37, 
+            x + 240, 
+            y + 75
+        ), "black")
+
+        draw.text((x + 140, y + 44), "Offline", "white", subSubFont)
+
+    else:
+        draw.text((x + 140, y + 44), "Online", "black", subSubFont)
 
 
 try:
-    asyncio.run(main())
+    main()
 except KeyboardInterrupt:
     print("Ctrl-C Entered")
     from waveshare_epd import epd7in5_V2 as disp
