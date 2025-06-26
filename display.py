9a299c4376ec615ab04086ae4578f5e612b4bc07
Hayden Steele <haydenallensteele@gmail.com>
2025-06-25 23:16:00 -0600

    a

--- display.py
+++ display.py
@@ -66,6 +66,10 @@ def main():
 
 
 def validFilePath(path_string: str):
+
+    print("Invalid Image Path")
+    print(path_string)
+
     if (not path_string.endswith(".png")):
         return False
     
