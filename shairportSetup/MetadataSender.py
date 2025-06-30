8325ef74df0cbc3fae0efd0aeca5b9b48256c23f
Hayden Steele <haydenallensteele@gmail.com>
2025-06-29 18:53:23 -0600

    a

--- shairportSetup/MetadataSender.py
+++ shairportSetup/MetadataSender.py
@@ -46,7 +46,7 @@ def handle_text_packet(text):
                 metadata[key] = line[len(prefix):].strip()
 
     if all(k in metadata for k in ("title", "artist", "album")):
-        time.sleep(0.5)  # Allow image file to be written
+        # time.sleep(0.5)  # Allow image file to be written
         send_metadata(metadata)
 
 def send_metadata(metadata):
